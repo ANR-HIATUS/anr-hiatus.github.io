e3a6129caf8204b6dbbfc5eb08b7409b8652b4a4
Projet ANR HIATUS <72440641+ANR-HIATUS@users.noreply.github.com>
2020-12-09 15:52:02 +0100

    Update piedpage.js

--- js/piedpage.js
+++ js/piedpage.js
@@ -7,8 +7,8 @@ document.write("\
 					<div class='moduletable'>\
  						<div class='bannergroup'>\
 							<div class='banneritem'>\
-								<a href='https://www.anr.fr' target='_blank' rel='noopener noreferrer' title='ANR'>\
-									<img src='./fichiers/rien.jpg' alt=''>\
+								<a href='https://www.anr-hiatus.github.io' target='_blank' rel='noopener noreferrer' title='HIATUS'>\
+									<img src='./fichiers/hiatus.jpg' alt=''>\
 								</a>\
 								<div class='clr'></div>\
 							</div>\
